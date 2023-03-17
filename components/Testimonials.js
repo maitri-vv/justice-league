c57982a2b56875ad7acc6cc32f1d33ac060d48cb
Samridhi-Sinha <96681774+yash19sinha@users.noreply.github.com>
2023-03-17 15:46:24 +0530

    modified testimonials
    Signed-off-by: Samridhi-Sinha <96681774+yash19sinha@users.noreply.github.com>

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -4,16 +4,40 @@ import { images } from '@/next.config'
 const Testimonials = () => {
   return (
     <div>
-      <div className="card lg:card-side bg-base-100 shadow-xl">
-  
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
+    <div className="grid justify-center w-full pt-6 pb-6 bg-slate-800">
+      <div className="grid grid-cols-2 gap-5 m-5 justify-items-center xl:grid-cols-3 xl:max-w-screen-xl lg:max-w-screen-lg ">
+          <div className="shadow-xl card lg:card-side bg-base-100">
+        <figure><img src="/images/img.jpg" alt="Album"/></figure>
+        <div className="card-body">
+          <h2 className="card-title">New album is released!</h2>
+          <p>Click the button to listen on Spotiwhy app.</p>
+          <div className="justify-end card-actions">
+            <button className="btn btn-primary">Listen</button>
+          </div>
+        </div>
+      </div>
+      <div className="shadow-xl card lg:card-side bg-base-100">
+        <figure><img src="/images/img.jpg" alt="Album"/></figure>
+        <div className="card-body">
+          <h2 className="card-title">New album is released!</h2>
+          <p>Click the button to listen on Spotiwhy app.</p>
+          <div className="justify-end card-actions">
+            <button className="btn btn-primary">Listen</button>
+          </div>
+        </div>
+      </div>
+      <div className="shadow-xl card lg:card-side bg-base-100">
+        <figure><img src="/images/img.jpg" alt="Album"/></figure>
+        <div className="card-body">
+          <h2 className="card-title">New album is released!</h2>
+          <p>Click the button to listen on Spotiwhy app.</p>
+          <div className="justify-end card-actions">
+            <button className="btn btn-primary">Listen</button>
+          </div>
+        </div>
+      </div>
+                </div>
+            </div>
     </div>
   )
 }
