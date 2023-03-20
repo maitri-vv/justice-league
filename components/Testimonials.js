a69d92ac7c427f22fd0b2ca9e33c17b8fa9d6e9c
maitri.vv <1032201087@tcetmumbai.in>
2023-03-20 22:18:02 +0530

    Know your case updated
    landing page updates and KYC too

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -4,10 +4,9 @@ import { images } from '@/next.config'
 const Testimonials = () => {
   return (
     <div>
-     <div className="indicator justify-center w-full ">
-  <span className="indicator-item indicator-bottom indicator-center badge badge-secondary"></span>
-  <div className="grid w-16 h-16 bg-base-300 place-items-center">Testimonials</div>
-</div>
+     <h2 className="text-3xl text-black font-bold items-center text-center">
+      Testimonials<div className="badge badge-primary text-xs badge-top">3</div>
+    </h2>
     <div className="grid justify-center w-full pt-6 pb-6 bg-slate-800">
       <div className="grid grid-cols-2 gap-5 m-5 justify-items-center xl:grid-cols-3 xl:max-w-screen-xl lg:max-w-screen-lg ">
           <div className="shadow-xl card lg:card-side bg-base-100">
@@ -16,7 +15,13 @@ const Testimonials = () => {
           <h2 className="card-title">New album is released!</h2>
           <p>Click the button to listen on Spotiwhy app.</p>
           <div className="justify-end card-actions">
-            <button className="btn btn-primary">Listen</button>
+          <div className="rating gap-1">
+  <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400" checked />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" />
+</div>
           </div>
         </div>
       </div>
@@ -26,7 +31,13 @@ const Testimonials = () => {
           <h2 className="card-title">New album is released!</h2>
           <p>Click the button to listen on Spotiwhy app.</p>
           <div className="justify-end card-actions">
-            <button className="btn btn-primary">Listen</button>
+          <div className="rating gap-1">
+  <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400" checked />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" />
+</div>
           </div>
         </div>
       </div>
@@ -36,7 +47,13 @@ const Testimonials = () => {
           <h2 className="card-title">New album is released!</h2>
           <p>Click the button to listen on Spotiwhy app.</p>
           <div className="justify-end card-actions">
-            <button className="btn btn-primary">Listen</button>
+          <div className="rating gap-1">
+  <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400" checked />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" />
+</div>
           </div>
         </div>
       </div>
--- pages/Domainnews.js
+++ pages/Domainnews.js
@@ -3,37 +3,180 @@ import { images } from '@/next.config'
 const Domainnews = () => {
   return (
     <div>
-      <div className="carousel w-full">
-  <div id="item1" className="carousel-item w-full max-w-md p-4 space-x-4 bg-neutral rounded-box">
-    <img src="/images/img.jpg" className="w-full" />
-  </div> 
-  <div id="item2" className="carousel-item w-full max-w-md p-4 space-x-4 bg-neutral rounded-box">
-    <img src="/images/img.jpg" className="w-full" />
-  </div> 
-  <div id="item3" className="carousel-item w-full max-w-md p-4 space-x-4 bg-neutral rounded-box">
-    <img src="/images/img.jpg" className="w-full" />
-  </div> 
-  <div id="item4" className="carousel-item w-full max-w-md p-4 space-x-4 bg-neutral rounded-box">
-    <img src="/images/img.jpg" className="w-full" />
+      <h2 className="text-4xl text-black font-bold items-center text-center">
+      Know Your Case
+    </h2>
+      <div className="carousel w-full ">
+  <div id="item1" className="carousel-item max-w-md p-4 space-x-4 rounded-box">
+  <div className="card w-96 glass">
+  <figure><img src="/images/img.jpg" alt="car!"/></figure>
+  <div className="card-body">
+    <h2 className="card-title">Right to equality</h2>
+    <p>  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+      irure dolor in reprehenderit in voluptate velit esse cillum
+      dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum. Sed ut perspiciatis unde omnis
+      iste natus error sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam, eaque ipsa quae ab illo
+      inventore veritatis et quasi architecto beatae vitae dicta
+      sunt explicabo.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Learn now!</button>
+    </div>
   </div>
-  <div id="item5" className="carousel-item w-full max-w-md p-4 space-x-4 bg-neutral rounded-box">
-    <img src="/images/img.jpg" className="w-full" />
   </div>
-  <div id="item6" className="carousel-item w-full max-w-md p-4 space-x-4 bg-neutral rounded-box">
-    <img src="/images/img.jpg" className="w-full" />
+  </div> 
+  <div id="item2" className="carousel-item max-w-md p-4 space-x-4 bg-neutral rounded-box">
+  <div className="card w-96 glass">
+  <figure><img src="/images/img.jpg" alt="car!"/></figure>
+  <div className="card-body">
+    <h2 className="card-title">Right to equality</h2>
+    <p>  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+      irure dolor in reprehenderit in voluptate velit esse cillum
+      dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum. Sed ut perspiciatis unde omnis
+      iste natus error sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam, eaque ipsa quae ab illo
+      inventore veritatis et quasi architecto beatae vitae dicta
+      sunt explicabo.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Learn now!</button>
+    </div>
   </div>
-  <div id="item7" className="carousel-item w-full max-w-md p-4 space-x-4 bg-neutral rounded-box">
-    <img src="/images/img.jpg" className="w-full" />
+  </div>
+  </div>
+  <div id="item3" className="carousel-item max-w-md p-4 space-x-4 rounded-box">
+  <div className="card w-96 glass">
+  <figure><img src="/images/img.jpg" alt="car!"/></figure>
+  <div className="card-body">
+    <h2 className="card-title">Right to equality</h2>
+    <p>  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+      irure dolor in reprehenderit in voluptate velit esse cillum
+      dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum. Sed ut perspiciatis unde omnis
+      iste natus error sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam, eaque ipsa quae ab illo
+      inventore veritatis et quasi architecto beatae vitae dicta
+      sunt explicabo.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Learn now!</button>
+    </div>
+  </div>
+  </div>
+  </div> 
+  <div id="item4" className="carousel-item max-w-md p-4 space-x-4 bg-neutral rounded-box">
+  <div className="card w-96 glass">
+  <figure><img src="/images/img.jpg" alt="car!"/></figure>
+  <div className="card-body">
+    <h2 className="card-title">Right to equality</h2>
+    <p>  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+      irure dolor in reprehenderit in voluptate velit esse cillum
+      dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum. Sed ut perspiciatis unde omnis
+      iste natus error sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam, eaque ipsa quae ab illo
+      inventore veritatis et quasi architecto beatae vitae dicta
+      sunt explicabo.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Learn now!</button>
+    </div>
+  </div>
+  </div>
+  </div>
+  <div id="item5" className="carousel-item max-w-md p-4 space-x-4 rounded-box">
+  <div className="card w-96 glass">
+  <figure><img src="/images/img.jpg" alt="car!"/></figure>
+  <div className="card-body">
+    <h2 className="card-title">Right to equality</h2>
+    <p>  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+      irure dolor in reprehenderit in voluptate velit esse cillum
+      dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum. Sed ut perspiciatis unde omnis
+      iste natus error sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam, eaque ipsa quae ab illo
+      inventore veritatis et quasi architecto beatae vitae dicta
+      sunt explicabo.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Learn now!</button>
+    </div>
+  </div>
+  </div>
+  </div>
+  <div id="item6" className="carousel-item max-w-md p-4 space-x-4 bg-neutral rounded-box">
+  <div className="card w-96 glass">
+  <figure><img src="/images/img.jpg" alt="car!"/></figure>
+  <div className="card-body">
+    <h2 className="card-title">Right to equality</h2>
+    <p>  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+      irure dolor in reprehenderit in voluptate velit esse cillum
+      dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum. Sed ut perspiciatis unde omnis
+      iste natus error sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam, eaque ipsa quae ab illo
+      inventore veritatis et quasi architecto beatae vitae dicta
+      sunt explicabo.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Learn now!</button>
+    </div>
+  </div>
+  </div>
+  </div>
+  <div id="item7" className="carousel-item max-w-md p-4 space-x-4 rounded-box">
+  <div className="card w-96 glass">
+  <figure><img src="/images/img.jpg" alt="car!"/></figure>
+  <div className="card-body">
+    <h2 className="card-title">Right to equality</h2>
+    <p>  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+      irure dolor in reprehenderit in voluptate velit esse cillum
+      dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum. Sed ut perspiciatis unde omnis
+      iste natus error sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam, eaque ipsa quae ab illo
+      inventore veritatis et quasi architecto beatae vitae dicta
+      sunt explicabo.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Learn now!</button>
+    </div>
+  </div>
+  </div>
   </div>
 </div> 
 <div className="flex justify-center w-full py-2 gap-2">
-  <a href="#item1" className="btn btn-md">1</a> 
+  <a href="#item1" className="btn btn-md btn-primary">1</a> 
   <a href="#item2" className="btn btn-md">2</a> 
-  <a href="#item3" className="btn btn-md">3</a> 
+  <a href="#item3" className="btn btn-md btn-primary">3</a> 
   <a href="#item4" className="btn btn-md">4</a>
-  <a href="#item5" className="btn btn-md">5</a>
+  <a href="#item5" className="btn btn-md btn-primary">5</a>
   <a href="#item6" className="btn btn-md">6</a>
-  <a href="#item7" className="btn btn-md">7</a>
+  <a href="#item7" className="btn btn-md btn-primary">7</a>
       
 </div>
 <div className="hero min-h-screen" style={{ backgroundImage: `url("/images/img.jpg")` }}>
@@ -46,6 +189,7 @@ const Domainnews = () => {
     </div>
   </div>
 </div>
+
 </div>
   )
 }
