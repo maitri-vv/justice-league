e11cbb3281fc666c4a0761d95534b722f0a3876f
maitri-vv <maitrivaghasiya99@gmail.com>
2023-03-30 19:38:38 +0530

    Delete backend directory

--- backend/routes/Newsletter/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'); 
-const router = express.Router(); 
-
-//importing routes 
-const getuser = require('./getUser');
-router.use('/user',getuser); 
-
-module.exports = router; 
