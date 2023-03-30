28c9b15ca4522c7e82d1ba54637f0ecbfce7a7c9
maitri.vv <1032201087@tcetmumbai.in>
2023-03-30 11:41:38 +0530

    imgsadded

--- components/Whatwedo.js
+++ components/Whatwedo.js
@@ -10,7 +10,7 @@ const Whatwedo = () => {
     <div className="grid justify-center w-full pt-6 pb-6 bg-slate-800">
       <div className="grid grid-cols-2 gap-5 m-5 justify-items-center xl:grid-cols-3 xl:max-w-screen-xl lg:max-w-screen-lg ">
           <div className="shadow-xl card lg:card-side bg-base-100">
-        <figure><img src="/images/img.jpg" alt="Album"/></figure>
+        <figure><img src="/images/wwd1.jpg" alt="Album"/></figure>
         <div className="card-body">
           <h2 className="card-title">Our Mission</h2>
           <p>The mission of the Department of Justice is to uphold the rule of law, to keep our country safe, and to protect civil rights.</p>
@@ -26,7 +26,7 @@ const Whatwedo = () => {
         </div>
       </div>
       <div className="shadow-xl card lg:card-side bg-base-100">
-        <figure><img src="/images/img.jpg" alt="Album"/></figure>
+        <figure><img src="/images/wwd3.jpg" alt="Album"/></figure>
         <div className="card-body">
           <h2 className="card-title">Our Values</h2>
           <p>Independence and Impartiality. We work each day to earn the public’s trust by following the facts and the law wherever they may lead, without prejudice.</p>
@@ -42,7 +42,7 @@ const Whatwedo = () => {
         </div>
       </div>
       <div className="shadow-xl card lg:card-side bg-base-100">
-        <figure><img src="/images/img.jpg" alt="Album"/></figure>
+        <figure><img src="/images/wwd2.jpg" alt="Album"/></figure>
         <div className="card-body">
           <h2 className="card-title">Our principles</h2>
           <p>Excellence. We work every day to provide the highest levels of service to the American people and to be a responsible steward of the taxpayers’ dollars.</p>
