6fbeadc7773fb67c5ccefab849055be5039a3061
Vs-karma <81770024+Vs-karma@users.noreply.github.com>
2023-03-30 14:59:35 +0530

    newsletter build

--- pages/Contactus.js
+++ pages/Contactus.js
@@ -1,57 +1,124 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
+// import { useRouter } from "next/router";
+import { newsletter } from "@/urls";
+import { useRouter } from "next/router";
+
+// import newsletter from "@/backend/models/newsletter";
 // import { DatePicker } from '@/components/DatePicker'
 const Contactus = () => {
+  const router = useRouter();
+  const [error, setError] = useState("");
+  useEffect(() => {
+    document.title = "Justice League Newsletter";
+  }, []);
+  const [input, setInput] = useState({
+    email: "",
+    name: "",
+    complain: " ",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (input.email === "" || input.name === "") {
+      setError("Email and Name Required");
+      setTimeout(() => {
+        setError("");
+      }, 2000);
+      return;
+    }
+    const res = await fetch(newsletter, {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify(input),
+    });
+    // let {resp} = await axios.post(UserLogin , input)
+    const data = await res.json();
+    console.log(data);
+    if (data.success) {
+      let authToken = data.authToken;
+      localStorage.setItem("authToken", authToken);
+      router.push("/");
+    } else {
+      setError(data.msg);
+      setTimeout(() => {
+        setError("");
+      }, 2000);
+    }
+  };
+
+  const handleChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
   return (
     <div>
       <div className="min-h-screen hero bg-base-200">
         <div className="flex-col hero-content lg:flex-row-reverse">
-            <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Register Your Complaint here!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            </div>
-            <div className="flex-shrink-0 w-full max-w-sm shadow-2xl card bg-base-100">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">
+              Register Your Complaint here!
+            </h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="flex-shrink-0 w-full max-w-sm shadow-2xl card bg-base-100">
             <div className="card-body">
-                <div className="form-control">
-                  <label className="label">
-                      <span className="label-date">Date</span>
-                  </label>
-                  <input type="date" placeholder="Date" className="input input-bordered" />
-                </div>
-                <div className="form-control">
+              {/* <div className="form-control">
                 <label className="label">
-                    <span className="label-text">Name</span>
+                  <span className="label-date">Date</span>
                 </label>
-                <input type="text" placeholder="Name" className="input input-bordered" />
-                </div>
-                <div className="form-control">
+                <input type="date" placeholder="Date" className="input input-bordered" />
+              </div> */}
+              <div className="form-control">
                 <label className="label">
-                    <span className="label-text">Email</span>
+                  <span className="label-text">Name</span>
                 </label>
-                <input type="email" placeholder="email" className="input input-bordered" />
-                </div>
-                <div className="form-control">
+                <input
+                  type="text"
+                  placeholder="Name"
+                  name="name"
+                  required onChange={(e) => handleChange(e)}
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
                 <label className="label">
-                    <span className="label-text">Number</span>
+                  <span className="label-text">Email</span>
                 </label>
-                <input type="number" placeholder="Number" className="input input-bordered" />
-                </div>
-
-                
-                <div className="form-control">
+                <input
+                  type="email"
+                  placeholder="email"
+                  name="email"
+                  required onChange={(e) => handleChange(e)}
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
                 <label className="label">
-                    <span className="label-text">Complaint</span>
+                  <span className="label-text">Complaint</span>
                 </label>
-                <textarea type="text" placeholder="Complaint" className="h-32 resize-none input input-bordered" />
-                </div>
-                <div className="mt-6 form-control">
-                <button className="btn btn-primary">Submit</button>
-                </div>
+                <textarea
+                  type="text"
+                  placeholder="Complaint"
+                  name="complain"
+                  required onChange={(e) => handleChange(e)}
+                  className="h-32 resize-none input input-bordered"
+                />
+              </div>
+              <div className="mt-6 form-control">
+                <button className="btn btn-primary" onClick={(e) => handleSubmit(e)}>Submit</button>
+              </div>
             </div>
-            </div>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contactus
+export default Contactus;
